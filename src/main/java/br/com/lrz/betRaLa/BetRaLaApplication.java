3a05817c05661f4d9e7ac9fb8eb99c1ad1a0285c
Lara Rodrigues <lararods73@gmail.com>
2023-10-27 18:35:04 -0300

    Initial commit

---
+++ src/main/java/br/com/lrz/betRaLa/BetRaLaApplication.java
@@ -0,0 +1,13 @@
+package br.com.lrz.betRaLa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetRaLaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetRaLaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lrz/betRaLa/BetRaLaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lrz.betRaLa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetRaLaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
