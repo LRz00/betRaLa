d9e14d523034e079c9d1cbf32012e4044a9dfb53
Lara Rodrigues <98907219+LRz00@users.noreply.github.com>
2023-11-11 20:54:24 -0300

    cria a view que registra times
    no sistema + o controller facade que invocará todos os metodos do sistema

---
+++ src/main/java/br/com/lrz/betRaLa/infrastructure/service/Facade.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.lrz.betRaLa.infrastructure.service;
+
+import br.com.lrz.betRaLa.time.model.Time;
+import br.com.lrz.betRaLa.time.service.IServiceTime;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+public class Facade implements IFacade {
+    //###################TIME###################
+    @Autowired
+    IServiceTime serviceTime;
+
+    @Override
+    public void salvarTime(Time time) {
+        serviceTime.saveTime(time);
+    }
+    
+    
+
+}
---
+++ src/main/java/br/com/lrz/betRaLa/infrastructure/service/IFacade.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.com.lrz.betRaLa.infrastructure.service;
+
+import br.com.lrz.betRaLa.time.model.Time;
+
+public interface IFacade {
+    //###################TIME###################
+
+    public abstract void salvarTime(Time time);
+}
