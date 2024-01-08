f48fc046fa91710b7d5b487ede7a43bcf87d20b5
Lara Rodrigues <lararods73@gmail.com>
2024-01-08 20:00:39 -0300

    atualizando controller de user
    fix: delete e findByCpf.
    new: updateSaldo e getAll

--- src/main/java/br/com/lrz/betRaLa/controllers/UserController.java
+++ src/main/java/br/com/lrz/betRaLa/controllers/UserController.java
@@ -6,7 +6,9 @@ package br.com.lrz.betRaLa.controllers;
 
 import br.com.lrz.betRaLa.models.User;
 import br.com.lrz.betRaLa.services.IServiceUser;
+import java.math.BigDecimal;
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +16,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -32,7 +37,7 @@ public class UserController {
     IServiceUser userService;
     
     @GetMapping("/{cpf}")
-    public ResponseEntity findByCpf(Long cpf){
+    public ResponseEntity findByCpf(@PathVariable Long cpf){
         User user = this.userService.findByCpf(cpf);
         return ResponseEntity.ok().body(user);
     }
@@ -51,10 +56,23 @@ public class UserController {
     
     //delete
     @DeleteMapping("/{cpf}")
-    public void delete (User user){
-        User deletableUser = (User) this.userService.findByCpf(user.getCpf());
+     public ResponseEntity<Void> delete(@PathVariable Long cpf){
+         User deletable = this.userService.findByCpf(cpf);        
+        this.userService.delete(deletable);
+        return ResponseEntity.noContent().build();
     }
     //update
+
+    @PutMapping("/updateSaldo")
+    public ResponseEntity<Void> updateSaldo(@RequestParam BigDecimal value, @RequestParam Long cpf) {
+        this.userService.updateSaldo(value, cpf);
+        return ResponseEntity.noContent().build();
+    }
     
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getAllUsers() {
+    List<User> users = this.userService.getAll();
+    return ResponseEntity.ok().body(users);
+}
     
 }
